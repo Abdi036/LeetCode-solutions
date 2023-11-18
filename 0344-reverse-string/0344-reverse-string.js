d247a23b9794dfd729344a60924f37d1e37dd7a2
Abdi Kumela <150321375+Abdi036@users.noreply.github.com>
2023-11-18 06:48:41 -0800

    Time: 81 ms (46.27%), Space: 49.7 MB (24.18%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,8 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+function reverseString(s) {
+    const str = s.reverse();
+    s = str;
+};
