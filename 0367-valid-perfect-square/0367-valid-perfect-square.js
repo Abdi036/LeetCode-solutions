39bf0ac46f0c262a77ad87ce60c5a2be9be60798
Abdi Kumela <150321375+Abdi036@users.noreply.github.com>
2023-11-18 07:30:30 -0800

    Time: 47 ms (82.29%), Space: 42.2 MB (18.84%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    let power = num ** (1/2);
+     const Floor = Math.floor(power);
+    return (Floor * Floor )=== num ? true : false;
+};
