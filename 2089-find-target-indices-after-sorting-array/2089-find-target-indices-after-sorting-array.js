3b458d2d19d9fc555338c8cf4aba76fb27620700
Abdi Kumela <150321375+Abdi036@users.noreply.github.com>
2023-11-27 12:03:41 -0800

    Time: 64 ms (20.93%), Space: 43.9 MB (37.36%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.js
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.js
@@ -5,8 +5,7 @@
  */
 function targetIndices(nums, target) {
     let arr=[];
-    let sorted = nums.sort((a,b) => a- b);
-    sorted.filter(function(val,i){
+    let sorted = nums.sort((a,b) => a- b).filter(function(val,i){
         if(val === target) arr.push(i);
     })
    return arr;
