dd967f64fc7d6375d30581d653d8dbfbba30f8ff
Abdi Kumela <150321375+Abdi036@users.noreply.github.com>
2024-01-08 12:51:44 -0800

    Time: 40 ms (76.10%), Space: 60.8 MB (51.68%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if(nums.length < 2) return 0;
+        Arrays.sort(nums);
+        int gap = 0;
+        
+        for(int i = 0; i < nums.length - 1; i++){
+            int curgap = nums[i + 1] - nums[i];
+            gap = Math.max(gap, curgap);
+        }
+        return gap;
+    }
+}
+
+
+
+ 
