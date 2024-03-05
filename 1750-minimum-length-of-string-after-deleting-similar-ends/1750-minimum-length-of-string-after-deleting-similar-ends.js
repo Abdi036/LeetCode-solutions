79a20b818e2210c95d2cc41e47e952a35631860f
Abdi Kumela <abdikumela036@gmail.com>
2024-03-04 23:43:06 -0800

    Time: 50 ms (97.37%), Space: 53 MB (65.79%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function minimumLength(s) {
+     let prefix = 0;
+     let suffix = s.length - 1;
+
+     while(prefix < suffix && s[prefix] === s[suffix]){
+         let elet = s[prefix];
+         while(prefix <= suffix && s[prefix] === elet){
+             prefix++
+         }
+         while(suffix >= prefix && s[suffix] === elet){
+             suffix--
+         }
+     }
+     return suffix - prefix + 1;
+
+};
