798c308d5401592c82dfb0ad82ebf2c19d7a3a34
Abdi Kumela <abdikumela036@gmail.com>
2024-03-13 22:59:37 +0300

    Time: 45 ms (97.80%), Space: 50.6 MB (68.99%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} path
+ * @return {string}
+ */
+function simplifyPath(path) {
+    let container = [];
+    let str = path.split("/")
+
+for(let i = 0; i < str.length; i++){
+    if(str[i] === "." || !str[i]) continue;
+    if(str[i] === "..") {
+        if(container.length > 0){
+            container.pop();
+        }
+    }else{
+        container.push(str[i]);
+    }
+}
+
+let myPath = container.join("/");
+    return "/"+ myPath; 
+};
