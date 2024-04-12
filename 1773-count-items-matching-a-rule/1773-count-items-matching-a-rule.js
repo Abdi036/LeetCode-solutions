0534bd5570bfcab9cfd1491627f72e3f081c8f01
Abdi Kumela <abdikumela036@gmail.com>
2024-04-12 19:18:19 +0300

    Time: 63 ms (70.27%), Space: 52.6 MB (77.13%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.js
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.js
@@ -6,15 +6,24 @@
  */
 function countMatches(items, ruleKey, ruleValue) {
     let count = 0;
+    const hashMap = new Map();
+    hashMap.set("type",0)
+    hashMap.set("color",1)
+    hashMap.set("name",2)
+
     for(let i = 0; i < items.length; i++){
-        if(ruleKey === "type"){
-            items[i][0] === ruleValue && count++;
-        }
-        else if(ruleKey === "color"){
-            items[i][1] === ruleValue && count++;
-        }else{
-            items[i][2] === ruleValue && count++;
-        }
+        if(items[i][hashMap.get(ruleKey)] === ruleValue) count++
     }
     return count;
 };
+
+// optional 
+
+        // if(ruleKey === "type"){
+        //     items[i][0] === ruleValue && count++;
+        // }
+        // else if(ruleKey === "color"){
+        //     items[i][1] === ruleValue && count++;
+        // }else{
+        //     items[i][2] === ruleValue && count++;
+        // }
