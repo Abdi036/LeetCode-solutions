ded6f964f72474a769d7abbf155775d982395b9f
Abdi Kumela <abdikumela036@gmail.com>
2024-11-08 15:53:41 +0300

    Time: 32 ms (18.26%), Space: 54.1 MB (35.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,9 +3,8 @@
  * @param {string} t
  * @return {boolean}
  */
-function isAnagram(s, t) {
-    // const str1 = s.split('').sort();
-    // const str2 = t.split('').sort();
-    // return str1.join('') === str2.join('') ? true : false;
-return s.split('').sort().join('') === t.split('').sort().join('') ? true : false;
+var isAnagram = function(s, t) {
+    let sSorted = s.split("").sort().join("")
+    let tSorted = t.split("").sort().join("")
+  return sSorted === tSorted
 };
