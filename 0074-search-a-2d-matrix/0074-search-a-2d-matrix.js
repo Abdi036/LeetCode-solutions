fc1ca051439d9037de68f2bfe2320ebe29726dc8
Abdi Kumela <abdikumela036@gmail.com>
2024-11-20 13:28:36 +0300

    Time: 0 ms (100%), Space: 49.3 MB (26.01%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -3,8 +3,30 @@
  * @param {number} target
  * @return {boolean}
  */
-function searchMatrix(matrix, target) {
-    let arr = matrix.flat();
-     if(arr.includes(target)) return true;
-     else return false;
+var searchMatrix = function(matrix, target) {
+    function BS(arr,target){
+        let left = 0
+        let right = arr.length - 1;
+
+
+        while(left <= right){
+            let mid = Math.floor((left + right)/2)
+            if(arr[mid] === target){ 
+                return true
+            }
+            else if(arr[mid] < target){
+                left = mid + 1
+            }else{
+                right = mid - 1
+            }
+        }
+        return false
+    }
+
+    for(let i = 0; i < matrix.length; i++){
+        if(BS(matrix[i],target)){
+          return true
+        }
+    }
+    return false
 };
