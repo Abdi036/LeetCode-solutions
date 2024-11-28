43609478fb97cc21e15abc382b50ecebc28b1bf4
Abdi Kumela <abdikumela036@gmail.com>
2024-11-28 21:21:29 +0300

    Time: 0 ms (100%), Space: 49 MB (96.57%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -3,14 +3,14 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-function intersection(nums1, nums2) {
-    const numSet1 = new Set(nums1);
-    const numSet2 = new Set(nums2);
-    let intrPoint = [];
-    for(const num of numSet2){
-        if(numSet1.has(num)){
-            intrPoint.push(num);
+var intersection = function(nums1, nums2) {
+    const res = []
+
+    for(let i = 0; i < nums1.length; i++){
+        if(nums2.includes(nums1[i]) && !res.includes(nums1[i])){
+            res.push(nums1[i])
         }
     }
-    return intrPoint;
+    return res
+
 };
