d3f1c7f01342c2ddb90755d2535e8b9870bef8d9
Abdi Kumela <abdikumela036@gmail.com>
2024-11-28 20:58:43 +0300

    Time: 5 ms (53.66%), Space: 52.4 MB (53.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -2,7 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
- function majorityElement(nums) {
-    nums.sort((a,b) => a - b);
-    return nums[Math.floor((nums.length / 2))];
+var majorityElement = function(nums) {
+    let hashMap = new Map()
+
+    for(let i = 0; i < nums.length; i++){
+        if(hashMap.has(nums[i])){
+            hashMap.set(nums[i],hashMap.get(nums[i]) + 1)
+        }else{
+            hashMap.set(nums[i],1)
+        }
+    }
+    let max = 0
+    let majorityElement = 0
+    for(let [key,value] of hashMap){
+        if(value > max){
+            max = value
+            majorityElement = key
+        }
+    }
+
+    return majorityElement
 };
