d1fe5d6d2ea5d4f3d9ba68fb20df8f2c845cb75d
Abdi Kumela <abdikumela036@gmail.com>
2025-01-06 21:02:43 +0300

    Time: 1 ms (48.23%), Space: 56.3 MB (58.6%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -2,7 +2,13 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-function reverseString(s) {
-    const str = s.reverse();
-    s = str;
+var reverseString = function(s) {
+    let left = 0  
+    let right = s.length - 1
+
+    while(left <= right){
+       [s[left],s[right]] = [s[right],s[left]]
+        left++
+        right--
+    }
 };
