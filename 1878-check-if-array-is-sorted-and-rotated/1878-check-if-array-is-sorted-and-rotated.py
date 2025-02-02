b7707e72bdaa6c8c4cf0fde2e43f5bad808eaf64
Abdi Kumela <abdikumela036@gmail.com>
2025-02-02 08:17:52 -1200

    Time: 0 ms (100%), Space: 17.6 MB (81.34%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.py
@@ -2,9 +2,6 @@ class Solution:
     def check(self, nums: List[int]) -> bool:
         sortedNums = sorted(nums)
 
-        if sortedNums == nums:
-            return True
-        
         for i in range(len(nums)):
             last = nums.pop()
             nums.insert(0, last)
