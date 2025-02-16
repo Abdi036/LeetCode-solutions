a2fb1afc9c878d9732ac335d2ff59dac590605da
Abdi Kumela <abdikumela036@gmail.com>
2025-02-15 19:34:36 -1200

    Time: 3 ms (45.97%), Space: 17.8 MB (52.23%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -1,18 +1,32 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        hashMap = {}
-
-        for num in arr:
-            if num in hashMap:
-                hashMap[num] += 1
-            else:
-                hashMap[num] = 1
-
-        for num in arr:
-            if num != 0 and (2 * num) in hashMap:
-                return True
-            if num == 0 and hashMap[num] > 1:
-                return True
-        return False
+        # [2,3,5,10]
+        arr.sort()
         
+        def BS(nums,tar):
+            l = 0
+            r = len(arr) - 1
 
+            while l <= r:
+                mid = (l + r) // 2
+
+                if(nums[mid] == tar):
+                    return mid
+                elif(nums[mid] > tar):
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return -1
+
+
+# [-19,-8,-2,0,4,6,10]
+        for i in range(len(arr)):
+            target = (arr[i] * 2)
+            res = BS(arr,target)
+            
+            if(res >= 0 and res != i):
+                return True
+            else:
+                continue
+        return False
+            
