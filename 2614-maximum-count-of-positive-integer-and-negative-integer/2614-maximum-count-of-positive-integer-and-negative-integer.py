7fa59e215e4b9c46974875b8b48297c6a1907d6b
Abdi Kumela <abdikumela036@gmail.com>
2025-03-12 12:24:37 +0300

    Time: 0 ms (100%), Space: 18 MB (69.11%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,12 +1,5 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        neg,pos = 0,0
-
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                continue
-            elif nums[i] > 0:
-                pos+=1
-            else:
-                neg+=1
-        return max(pos,neg)
+        neg = bisect_left(nums,0)
+        pos = len(nums) - bisect_right(nums,0)
+        return max(neg,pos)
