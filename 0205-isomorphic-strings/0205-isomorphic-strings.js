b89ff3b95714bb7f024b2c2494eeb6decd1a4581
Abdi Kumela <abdikumela036@gmail.com>
2025-09-19 12:07:33 +0300

    Time: 8 ms (35.11%), Space: 55.9 MB (32.38%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,22 +4,20 @@
  * @return {boolean}
  */
 function isIsomorphic(s, t) {
-    const hashMapST = new Map();
-    const hashMapTS = new Map();
-    
+    const hashMapST = new Map()
+    const hashMapTS = new Map()
     let char1;
     let char2;
-    
+
     for(let i = 0; i < s.length; i++){
-        char1 = s[i]
-        char2 = t[i]
-        
-        if(hashMapST.has(char1) && hashMapST.get(char1) !== char2 || hashMapTS.has(char2) && hashMapTS.get(char2) !== char1){
+        char1 = s[i];
+        char2 = t[i];
+
+        if(hashMapST.has(char1) && hashMapST.get(char1) !== char2 ||hashMapTS.has(char2) && hashMapTS.get(char2) !== char1 ) {
             return false;
         }
-        
         hashMapST.set(char1,char2)
         hashMapTS.set(char2,char1)
     }
- return true
+    return true;
 };
