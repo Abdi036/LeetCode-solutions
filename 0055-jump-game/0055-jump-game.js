4d8ee6e19c5aa42d5ff41317df33fd8bdd8cebdd
Abdi Kumela <abdikumela036@gmail.com>
2025-09-26 21:51:31 +0300

    Time: 0 ms (100%), Space: 58.5 MB (71.82%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function canJump(nums) {
+    let target = nums.length - 1;
+    for (let i = (nums.length-1); i>=0; i--)
+    {
+        if (nums[i]+i >= target)
+            target = i;
+    }
+    if (target == 0)
+        return true;
+    return false;
+    
+};
