74f143b465556fd79fa56cb6c44c1a753add4d08
Abdi Kumela <abdikumela036@gmail.com>
2025-10-26 12:45:44 +0300

    Time: 0 ms (100%), Space: 53.4 MB (89.81%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    if(!root) return []
+    const res = []
+    function traverse(curNode){
+        res.push(curNode.val)
+        if(curNode.left) traverse(curNode.left)
+        if(curNode.right) traverse(curNode.right)
+    }
+    traverse(root)
+    return res
+};
