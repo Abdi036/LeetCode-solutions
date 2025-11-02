ead3dc1f960fdc85133064fb35c7163f16c271a0
Abdi Kumela <abdikumela036@gmail.com>
2025-11-02 22:27:16 +0300

    Time: 2 ms (45.04%), Space: 54.9 MB (81.49%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,13 +4,12 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    const res = []
+  const res = []
 
     for(let i = 0; i < nums1.length; i++){
         if(nums2.includes(nums1[i]) && !res.includes(nums1[i])){
             res.push(nums1[i])
         }
     }
-    return res
-
+    return res  
 };
